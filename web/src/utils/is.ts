5cb2576a3b77e9ee80d03a4781a60503ab8b005b
xingkoo <xingkoo@gmail.com>
2026-09-26 17:20:59 +0800

    refactor(standard-v1): use unknown guards and honor primitive and boolean contracts

--- web/src/utils/is.ts
+++ web/src/utils/is.ts
@@ -1,53 +1,53 @@
 const opt = Object.prototype.toString;
 
-export function isArray(obj: any): obj is any[] {
-  return opt.call(obj) === '[object Array]';
+export function isArray(obj: unknown): obj is unknown[] {
+  return Array.isArray(obj);
 }
 
-export function isObject(obj: any): obj is { [key: string]: any } {
-  return opt.call(obj) === '[object Object]';
+export function isObject(obj: unknown): obj is Record<string, unknown> {
+  return typeof obj === 'object' && obj !== null && opt.call(obj) === '[object Object]';
 }
 
-export function isString(obj: any): obj is string {
-  return opt.call(obj) === '[object String]';
+export function isString(obj: unknown): obj is string {
+  return typeof obj === 'string';
 }
 
-export function isNumber(obj: any): obj is number {
-  return opt.call(obj) === '[object Number]' && obj === obj; // eslint-disable-line
+export function isNumber(obj: unknown): obj is number {
+  return typeof obj === 'number' && !Number.isNaN(obj);
 }
 
-export function isRegExp(obj: any) {
+export function isRegExp(obj: unknown): boolean {
   return opt.call(obj) === '[object RegExp]';
 }
 
-export function isFile(obj: any): obj is File {
+export function isFile(obj: unknown): obj is File {
   return opt.call(obj) === '[object File]';
 }
 
-export function isBlob(obj: any): obj is Blob {
+export function isBlob(obj: unknown): obj is Blob {
   return opt.call(obj) === '[object Blob]';
 }
 
-export function isUndefined(obj: any): obj is undefined {
+export function isUndefined(obj: unknown): obj is undefined {
   return obj === undefined;
 }
 
-export function isNull(obj: any): obj is null {
+export function isNull(obj: unknown): obj is null {
   return obj === null;
 }
 
-export function isFunction(obj: any): obj is (...args: any[]) => any {
+export function isFunction(obj: unknown): obj is (...args: never[]) => unknown {
   return typeof obj === 'function';
 }
 
-export function isEmptyObject(obj: any): boolean {
+export function isEmptyObject(obj: unknown): boolean {
   return isObject(obj) && Object.keys(obj).length === 0;
 }
 
-export function isExist(obj: any): boolean {
-  return obj || obj === 0;
+export function isExist(obj: unknown): boolean {
+  return Boolean(obj) || obj === 0;
 }
 
-export function isWindow(el: any): el is Window {
-  return el === window;
+export function isWindow(el: unknown): el is Window {
+  return typeof window !== 'undefined' && el === window;
 }
